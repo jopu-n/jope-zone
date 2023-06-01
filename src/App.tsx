88e212437bbb27ee6a6665610b7a0c2bc8620981
Johannes Natunen <johannes.natunen@edu.turkuamk.fi>
2023-06-01 11:06:54 +0300

    add jope image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import jope from "./assets/jope.png";
+
 function App() {
   return (
     <>
-      <div className="relative">
-        <div className="flex flex-wrap items-center">
+      <div className="flex h-screen">
+        <div className="m-auto">
           <p className="font-mono font-bold italic">Jope Zone</p>
+          <img src={jope} alt="Jope" />
         </div>
       </div>
     </>
